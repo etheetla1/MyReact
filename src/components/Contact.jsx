e564d8bacb04ef6a23d3600ddd76a49fc6968576
etheetla1 <etheetla1@student.gsu.edu>
2025-04-10 11:08:48 -0400

    Daily update: Thu Apr 10 11:08:48 EDT 2025

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import { CONTACT } from "../constants";
+
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+      <div className="text-center tracking-tighter">
+        <p className="my-4">{CONTACT.address}</p>
+        <p className="my-4">{CONTACT.phoneNo}</p>
+        <a
+          href={`mailto:${CONTACT.email}`}
+          className="border-b border-neutral-500 pb-1 text-sm"
+        >
+          {CONTACT.email}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
