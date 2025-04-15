812f09106661a750c8502611e1bff8ac1b6485f2
etheetla1 <etheetla1@student.gsu.edu>
2025-04-15 18:34:34 -0400

    Daily update: Tue Apr 15 18:34:34 EDT 2025

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/elishaTheetlaProfile.png";
+import profilePic from "../assets/elishaTheetlaProfile1.png";
 import { motion } from "framer-motion";
 
 const container = (delay = 0) => ({
